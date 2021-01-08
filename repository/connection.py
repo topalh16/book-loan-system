24057c60d00151baa97a8f77a4a3893a4ee93f1b
topalh16 <topalh16@itu.edu.tr>
2021-01-08 22:12:47 +0300

    Update connection string

--- repository/connection.py
+++ repository/connection.py
@@ -9,5 +9,4 @@ def get_db():
     if url is None:
         return postgresql.open('pq://postgres:Gandalf01@127.0.0.1:5432/book_loan')
     else:
-        print(connection_string)
-        return postgresql.open(connection_string)
+        return postgresql.open(url)
